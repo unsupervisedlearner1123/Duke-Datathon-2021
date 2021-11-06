b774ea31686d44e96f2e3452455c563c8f2cdb5e
Moritz Wilksch <moritzwilksch@gmail.com>
2021-11-06 16:52:53 -0400

    clustering experiments

--- 10_scripts/exp_clustering.py
+++ 10_scripts/exp_clustering.py
@@ -18,13 +18,31 @@ subset = df[(col for col in q_cols if col not in kick_out)].copy()
 for col in subset.columns:
     subset[col] = subset[col].cat.add_categories(["missing"])
     subset[col] = subset[col].fillna("missing")
-    
+
 
 #%%
 
 from sklearn.preprocessing import OrdinalEncoder
 
 oe = OrdinalEncoder()
-oe.fit_transform(subset)
+subset_encoded = oe.fit_transform(subset)
+subset_encoded = pd.DataFrame(subset_encoded, columns=subset.columns)
 
 #%%
+from sklearn.cluster import KMeans
+
+inertias = []
+for n_clus in range(2, 10):
+    km = KMeans(n_clusters=n_clus, random_state=42)
+    km.fit(subset_encoded)
+    inertias.append(km.inertia_)
+
+#%%
+plt.plot(range(2, 10), inertias)
+
+#%%
+km = KMeans(n_clusters=4, random_state=42)
+preds = km.fit_predict(subset_encoded)
+
+subset_encoded.groupby(preds).mean().style.background_gradient(cmap='coolwarm', axis=0)
+
