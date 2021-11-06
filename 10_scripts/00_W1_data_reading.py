9c3384deeb1f29402a4a93c3b21df9bd80ea8d46
ad522 <ad522@duke.edu>
2021-11-06 15:59:57 -0400

    temp update

--- 10_scripts/00_W1_data_reading.py
+++ 10_scripts/00_W1_data_reading.py
@@ -4,10 +4,11 @@ import numpy as np
 import pyreadstat
 import os
 
-from data_mapping import merge_column_mapping
-from data_mapping import merge_response_mapping
+from data_mapping import w1_column_mapping
+from data_mapping import w1_response_mapping
 
 # %%
+# read in the data
 pd.set_option("display.max_rows", 100, "display.max_columns", 100)
 
 df1, meta = pyreadstat.read_sav(
@@ -15,8 +16,6 @@ df1, meta = pyreadstat.read_sav(
         "../00_source_data/W1 Merged Data/Wave.1_Data/Merge/Wave1_20170906.sav"
     )
 )
-# df1.head(10)
-# %%
 
 imp_vars = pd.read_csv(
     "https://raw.githubusercontent.com/DukeUndergraduateML/datathon2021/main/important-variables/D2-ABS1%2B2.csv",
@@ -25,33 +24,26 @@ imp_vars = pd.read_csv(
 imp_cols = imp_vars[imp_vars["ABS1_Coding_name"] != "na"]["ABS1_Coding_name"].tolist()
 
 #%%
+# remove duplicate columns
 kickout = ["se004", "country", "se002", "se003a", "se005"]
 imp_cols = [x for x in imp_cols if x not in kickout]
 
 # %%
+# pick out the important columns
 df = pd.concat([df1.iloc[:, :11], df1[imp_cols]], axis=1,)
-df.head()
-df.columns
-
 
 # %%
-for colname, unique_vals in merge_response_mapping.items():
+# rename categories and column names
+for colname, unique_vals in w1_response_mapping.items():
     df[colname] = df[colname].replace(unique_vals)
 
 # %%
-df.rename(merge_column_mapping, axis=1, inplace=True)
+df.rename(w1_column_mapping, axis=1, inplace=True)
 
 # %%
-df.head()
-# %%
-
 df = df.drop("idnumber", axis=1)
 
 # %%
-
-df.columns
-# %%
-
 df["educationyear"] = (
     df["educationyear"]
     .replace("less than 1 year", 0)
@@ -59,7 +51,6 @@ df["educationyear"] = (
     .str.replace(" years", "")
     .astype("float")
 )
-
 # %%
 cat_cols = [
     "country",
@@ -98,6 +89,7 @@ for catcol in cat_cols:
 
 for flcol in fl_cols:
     df[flcol] = df[flcol].astype("float")
+
 # %%
 df.to_parquet("../20_intermediate_files/mergeddata.parquet")
 # %%
