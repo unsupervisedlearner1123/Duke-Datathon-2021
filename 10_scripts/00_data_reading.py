6ad43fb04b01145c42a4a95d2d36128ef8624ec1
ad522 <ad522@duke.edu>
2021-11-06 14:42:59 -0400

    update object types

--- 10_scripts/00_data_reading.py
+++ 10_scripts/00_data_reading.py
@@ -4,19 +4,19 @@ import numpy as np
 import pyreadstat
 import os
 
-from data_mapping import column_mapping
-from data_mapping import response_mapping
+from data_mapping import merge_column_mapping
+from data_mapping import merge_response_mapping
 
-COUNTRY = "Japan"
-
-
+# %%
 pd.set_option("display.max_rows", 100, "display.max_columns", 100)
 
-df, meta = pyreadstat.read_sav(
-    os.path.join("../00_source_data/W1 Merged Data/Wave.1_Data/Japan/japan v4.2.sav")
+df1, meta = pyreadstat.read_sav(
+    os.path.join(
+        "../00_source_data/W1 Merged Data/Wave.1_Data/Merge/Wave1_20170906.sav"
+    )
 )
-# df.head(10)
-
+# df1.head(10)
+# %%
 
 imp_vars = pd.read_csv(
     "https://raw.githubusercontent.com/DukeUndergraduateML/datathon2021/main/important-variables/D2-ABS1%2B2.csv",
@@ -24,30 +24,53 @@ imp_vars = pd.read_csv(
 )
 imp_cols = imp_vars[imp_vars["ABS1_Coding_name"] != "na"]["ABS1_Coding_name"].tolist()
 
-# %%
-df2 = pd.concat([df.iloc[:, :11], df[imp_cols]], axis=1)
-df2.head()
-
-
 #%%
-for colname, unique_vals in response_mapping.items():
-    df2[colname] = df2[colname].replace(unique_vals)
+kickout = ["se004", "country", "se002", "se003a", "se005"]
+imp_cols = [x for x in imp_cols if x not in kickout]
 
 # %%
-df2.rename(column_mapping, axis=1, inplace=True)
+df = pd.concat([df1.iloc[:, :11], df1[imp_cols]], axis=1,)
+df.head()
+df.columns
+
+
+# %%
+for colname, unique_vals in merge_response_mapping.items():
+    df[colname] = df[colname].replace(unique_vals)
+
+# %%
+df.rename(merge_column_mapping, axis=1, inplace=True)
+
+# %%
+df.head()
+# %%
+
+df = df.drop("idnumber", axis=1)
+
+# %%
+
+df.columns
+# %%
+
+df["educationyear"] = (
+    df["educationyear"]
+    .replace("less than 1 year", 0)
+    .astype("string")
+    .str.replace(" years", "")
+    .astype("float")
+)
 
 # %%
 cat_cols = [
     "country",
-    "area",
     "urban_rural",
-    "grtokyo50",
-    "grkansai30",
-    "citysize",
-    "gender",
+    "agegroup",
+    "age",
     "maritalstatus",
+    "married",
     "education",
-    "income",
+    "gender",
+    "income_quantile",
     "q007",
     "q008",
     "q009",
@@ -65,24 +88,16 @@ cat_cols = [
     "q127",
 ]
 
-int_cols = [
-    "yrsurvey",
-    "month",
-    "day",
-    "time",
-    "age"
-]
+fl_cols = ["yrsurvey", "age", "educationyear"]
 
 
 #%%
 
-df2 = df2.drop("country", axis=1)
-df2['country'] = COUNTRY
-
 for catcol in cat_cols:
-    df2[catcol] = df2[catcol].astype("category")
+    df[catcol] = df[catcol].astype("category")
 
-for intcol in int_cols:
-    df2[intcol] = df2[intcol].astype("int")
-
+for flcol in fl_cols:
+    df[flcol] = df[flcol].astype("float")
+# %%
+df.to_parquet("../20_intermediate_files/mergeddata.parquet")
 # %%
