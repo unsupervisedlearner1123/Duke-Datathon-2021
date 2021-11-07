c427c753d4533475aca366b884ad7df3f034309e
unsupervisedlearner1123 <deekshitasaikia@gmail.com>
2021-11-06 21:27:13 -0400

    Extracting additional outcomes after w1w2 matching v2

--- 10_scripts/data_focus.py
+++ 10_scripts/data_focus.py
@@ -13,6 +13,12 @@ df1, meta = pyreadstat.read_sav(
     )
 )
 
+df2, meta2 = pyreadstat.read_sav(
+    os.path.join(
+        "../00_source_data/W2 Merged Data/2w-3rd_release_all/merge/Wave2_20170724.sav"
+    )
+)
+
 comp = pd.read_csv(
     "https://raw.githubusercontent.com/unsupervisedlearner1123/Duke-Datathon-2021/main/20_intermediate_files/w1_w2_mapping.txt?token=AQKRUJB4YQ6CMZVY7NXJXHTBSBIWC",
     names=["wave1", "wave2"],
@@ -27,3 +33,11 @@ col_list = [
     key for key, val in meta.column_names_to_labels.items() if val in meta_filter
 ]
 df_wave1 = df1[col_list]
+
+meta_filter2 = [
+    col for col in meta2.column_labels if col in comp["wave2"].str.strip().tolist()
+]
+col_list2 = [
+    key for key, val in meta2.column_names_to_labels.items() if val in meta_filter2
+]
+df_wave2 = df2[col_list2]
