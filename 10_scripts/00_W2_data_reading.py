13d92da6ebe6d3eea8451e2a97e5b630ec895a92
ad522 <ad522@duke.edu>
2021-11-06 20:14:42 -0400

    age group mapping

--- 10_scripts/00_W2_data_reading.py
+++ 10_scripts/00_W2_data_reading.py
@@ -32,17 +32,100 @@ imp_cols = [x for x in imp_cols if x not in kickout]
 # pick out the important columns
 df = pd.concat([df1.iloc[:, :11], df1[imp_cols]], axis=1,)
 
+#%%
+# extract the data mappings
+columns = dict()
+for k, v in meta.column_names_to_labels.items():
+    if k in df.columns:
+        columns[k] = v
+
+responses = dict()
+for k, v in meta.variable_value_labels.items():
+    if k in df.columns:
+        responses[k] = v
+
 # %%
 # rename categories and column names
 for colname, unique_vals in w2_response_mapping.items():
     df[colname] = df[colname].replace(unique_vals)
 
-# %%
 df.rename(w2_column_mapping, axis=1, inplace=True)
 
+# %%
+# drop or clean columns to change data type
+df = df.drop("idnumber", axis=1)
 
+#%%
+# Factorize age to match variables from W1
+df["temp"] = df["age"] == "REFUSED"
+df["agegroup"] = df["age"]
+df["age"] = df["age"].replace("REFUSED", np.nan).astype("float")
+bins = [0, 20, 25, 30, 35, 40, 45, 50, 55, 60, 71, 76, 200]
+labels = [
+    "18-19",
+    "20-25",
+    "25-29",
+    "30-34",
+    "35-39",
+    "40-44",
+    "45-49",
+    "50-54",
+    "55-59",
+    "60-70",
+    "71-75",
+    "76 & over",
+]
+df["agegroup"] = pd.cut(df["age"], bins=bins, labels=labels, right=False)
+df["agegroup"] = df["agegroup"].cat.add_categories("REFUSED")
+df["age"] = df["age"].astype("string")
+df.loc[df["temp"],"age"] = "REFUSED"
+df.loc[df["temp"],"agegroup"] = "REFUSED"
 
+#%%
+# set column data types
+cat_cols = [
+    "country",
+    "urban_rural",
+    "q1",
+    "q2",
+    "q3",
+    "q4",
+    "q5",
+    "q6",
+    "q7",
+    "q8",
+    "gender",
+    "maritalstatus",
+    "education",
+    "income_quintile",
+    "q9",
+    "q12",
+    "q13",
+    "q93",
+    "q130",
+    "q38",
+    "q40",
+    "q108",
+    "q110",
+    "q124",
+    "q126",
+    "q128",
+    "q32",
+    "q48",
+    "q49",
+    "q94",
+    "q103",
+    "q104",
+]
 
+for catcol in cat_cols:
+    df[catcol] = df[catcol].astype("category")
 
 
 # %%
+# export to parquet
+# df.to_parquet("../20_intermediate_files/W2mergeddata.parquet")
+
+# %%
+# done :)
+
